d6504662e8e3f6d4ccd3a2109c2a60bbea01d6f2
Brienne McNally <brienne.mcnally@gmail.com>
2018-03-01 16:08:54 -0800

    completed movie buff

--- movie_buff/movie_buff/02_queries.rb
+++ movie_buff/movie_buff/02_queries.rb
@@ -1,19 +1,53 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer      not null, primary key
+#  name        :string
+#
+# Table name: movies
+#
+#  id          :integer      not null, primary key
+#  title       :string
+#  yr          :integer
+#  score       :float
+#  votes       :integer
+#  director_id :integer
+#
+# Table name: castings
+#
+#  id          :integer      not null, primary key
+#  movie_id    :integer      not null
+#  actor_id    :integer      not null
+#  ord         :integer
+
+
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+  .select(:id, :title, :yr, :score)
+  .where(yr: 1980..1989, score: 3..5)
 end
+# .where(score: 3..5)
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+    .group(:yr)
+    .having('MAX(score) <= 8')
+    .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where("movies.title = ?", title)
+    .order('castings.ord')
 end
 
 def vanity_projects
@@ -22,11 +56,23 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie
+    .joins(:actors)
+    .select(:id, :title, 'actors.name')
+    .where('castings.ord = 1')
+    .where('castings.actor_id = movies.director_id')
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
+  Actor.
+    joins(:castings)
+    .select(:id, :name, 'COUNT(castings.ord) as roles')
+    .where('castings.ord > 1')
+    .group(:id)
+    .order('roles DESC')
+    .limit(2)
 
 end
+# 'COUNT(*)'
